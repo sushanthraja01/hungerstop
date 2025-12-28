1e36b268b1756edf5d9720a04dd7ee10e650c1a3
sushanthraja01 <rajasaidathasushanth2006@gmail.com>
2025-12-28 22:00:43 +0530

    Change API path to production URLs

--- src/helper/Api_path.js
+++ src/helper/Api_path.js
@@ -1,3 +1,3 @@
-export const api_path = "http://localhost:2006/"
+export const api_path = "https://backend-nodejs-hungerstop.onrender.com
 export const cldname = "https://res.cloudinary.com/dme2vkioe/image/upload/"
 
