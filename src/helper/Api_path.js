e5f522c33fb57f35a57a7f6283df0c1e4b4a0535
sushanthraja01 <rajasaidathasushanth2006@gmail.com>
2025-12-28 22:16:27 +0530

    Add trailing slash to api_path URL

--- src/helper/Api_path.js
+++ src/helper/Api_path.js
@@ -1,3 +1,3 @@
-export const api_path = "https://backend-nodejs-hungerstop.onrender.com"
+export const api_path = "https://backend-nodejs-hungerstop.onrender.com/"
 export const cldname = "https://res.cloudinary.com/dme2vkioe/image/upload/"
 
